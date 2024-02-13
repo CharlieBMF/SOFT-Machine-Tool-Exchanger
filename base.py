e120e1a1dbfe1b827d1b04fbee2aaa53e9a38445
CharlieBMF <marcinski.karol@gmail.com>
2024-02-13 09:43:58 +0100

    project

---
+++ base.py
@@ -0,0 +1,267 @@
+import datetime
+import re
+import pymcprotocol
+import ast
+import pyodbc
+
+
+class ToolLocation:
+    def __init__(self, location_name, location_ip, location_port, location_network, location_network_plc,
+                 location_id_line, location_id_machine):
+        self.location_name = location_name
+        self.location_ip = location_ip
+        self.location_port = location_port
+        self.location_network = location_network
+        self.location_network_plc = location_network_plc
+        self.location_id_line = location_id_line
+        self.location_id_machine = location_id_machine
+        self.machine = self.define_machine_root()
+
+    def define_machine_root(self):
+        pymc3e = pymcprotocol.Type3E()
+        if self.location_network and self.location_network_plc:
+            pymc3e.network = self.location_network
+            pymc3e.pc = self.location_network_plc
+        return pymc3e
+
+    def connect(self):
+        self.machine.connect(ip=self.location_ip, port=self.location_port)
+
+    def close_connection(self):
+        self.machine.close()
+
+    def read_bits(self, head, size=1):
+        return self.machine.batchread_bitunits(headdevice=head, readsize=size)
+
+    def read_words(self, head, size=1):
+        return self.machine.batchread_wordunits(headdevice=head, readsize=size)
+
+    def read_random_words(self, word_devices, double_word_devices):
+        return self.machine.randomread(word_devices=word_devices, dword_devices=double_word_devices)
+
+    def write_word(self, head, values):
+        self.machine.batchwrite_wordunits(headdevice=head, values=values)
+
+    def write_random_bits(self, bits: list, values: list):
+        self.machine.randomwrite_bitunits(bit_devices=bits, values=values)
+
+    def write_random_words(self, word_devices: list, word_values: list,
+                           double_word_devices: list, double_word_values: list):
+        self.machine.randomwrite(word_devices=word_devices, word_values=word_values, dword_devices=double_word_devices,
+                                 dword_values=double_word_values)
+
+
+class Tool(ToolLocation):
+    def __init__(self, location_name, location_ip, location_port, location_network, location_network_plc,
+                 location_id_line, location_id_machine, tool_addresses, conn):
+
+        super().__init__(location_name, location_ip, location_port, location_network, location_network_plc,
+                         location_id_line, location_id_machine)
+        self.tool_addresses = ast.literal_eval(tool_addresses)
+        self.conn_string = conn
+
+    def check_trigger(self):
+        self.connect()
+        response_trigger = self.read_bits(head=self.tool_addresses['R_Task_Request_M'])
+        response_task = self.read_words(head=self.tool_addresses['R_Task_No_W'])
+        self.close_connection()
+        return response_trigger[0], response_task[0]
+
+    def get_tool_name_PLC(self):
+        self.connect()
+        tool_name_decimal = (
+            self.read_words(self.tool_addresses['R_Tool_Code_ASCII'][0], len(self.tool_addresses['R_Tool_Code_ASCII'])))
+        self.close_connection()
+        tool_name_ascii = self.convert_to_ascii(tool_name_decimal)
+        return tool_name_ascii
+
+    def get_operator_name_PLC(self):
+        self.connect()
+        operator_name_decimal = (
+            self.read_words(self.tool_addresses['R_Operator_ASCII'][0], len(self.tool_addresses['R_Operator_ASCII'])))
+        self.close_connection()
+        operator_name_ascii = self.convert_to_ascii(operator_name_decimal)
+        return operator_name_ascii
+
+    def get_actual_counter_PLC(self):
+        self.connect()
+        actual_counter = self.read_random_words(
+            word_devices=[], double_word_devices=self.tool_addresses['R_Actual_Counter_DW'])
+        self.close_connection()
+        return actual_counter[1][0]
+
+    def get_tool_data_SQL(self, tool_name):
+        tool_type = tool_name.rsplit('-', 1)[0]
+        tool_number = tool_name.rsplit('-', 1)[1]
+        query = (f"SELECT DefID, TypeName, DefToolID, DefReplacementQty, DefProducedQty, DefConfirmation, DefRelease, "
+                 f"StatsProdAllowed, AdrIDLine, AdrIDMachine, StatsDescription "
+                 f"FROM vToolDefinitionFullView "
+                 f"WHERE TypeName = '{tool_type}' AND DefToolID = '{tool_number}'")
+        tool_data = self.execute_query(query, 'SELECT', 'one')
+        print('Tool data ', tool_data)
+        return tool_data[0]
+
+    def update_tool_data_SQL(self, tool_data, actual_counter):
+        current_datetime = datetime.datetime.now()
+        DefLastCounterUpdate = current_datetime.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        query = (f'UPDATE tToolDefinitions SET DefProducedQty = {actual_counter}, '
+                 f'DefLastCounterUpdate = \'{DefLastCounterUpdate}\' '
+                 f'WHERE DefID = {tool_data["DefID"]}')
+        self.execute_query(query, 'UPDATE')
+
+    def generate_writable_plc_response(self, tool_data, task, error):
+        if error == 0:
+            if task == 1:
+                response = self.generate_ok_response(tool_data, task)
+            if task == 2:
+                if (int(tool_data['DefProducedQty']) < int(tool_data['DefReplacementQty']) and
+                        tool_data['DefConfirmation'] and tool_data['DefRelease'] and tool_data['StatsProdAllowed']):
+                    # TOOL IS OK AND CAN BE USED TO PRODUCTION
+                    print('ALLOK')
+                    response = self.generate_ok_response(tool_data, task)
+
+                else:
+                    response = self.generate_ng_response(tool_data, task)
+        if error == 1:
+            response = self.generate_error_response()
+        return response
+
+    def generate_ok_response(self, tool_data, task):
+        keys = [writable for writable in self.tool_addresses.keys() if writable.startswith('W')]
+        if task == 1:
+            values = [1, 1, 0, 0, 0, 0, 0]
+        if task == 2:
+            values = [1, 1, 0, 1, 0, int(tool_data['DefProducedQty']), int(tool_data['DefReplacementQty'])]
+        response = dict(zip(keys, values))
+        return response
+
+    def generate_ng_response(self, tool_data, task):
+        if task == 2:
+            keys = [writable for writable in self.tool_addresses.keys() if writable.startswith('W')]
+            values = [1, 1, 0, 0, 1, int(tool_data['DefProducedQty']), int(tool_data['DefReplacementQty'])]
+            response = dict(zip(keys, values))
+            return response
+
+    def generate_error_response(self):
+        keys = [writable for writable in self.tool_addresses.keys() if writable.startswith('W')]
+        values = [1, 0, 1, 0, 0, 0, 0]
+        response = dict(zip(keys, values))
+        return response
+
+    def generate_reset_plc_signals(self, task):
+        if task == 1:
+            reseter = {
+                'R_Task_Request_M': 0, 'R_Task_No_W': 0,
+                'R_Tool_Code_ASCII': [0] * len(self.tool_addresses['R_Tool_Code_ASCII']),
+                'R_Actual_Counter_DW': [0],
+                'R_Operator_ASCII': [0] * len(self.tool_addresses['R_Operator_ASCII'])
+            }
+            print('\nRESET FOR DISASSEMBLY', reseter)
+        if task == 2:
+            reseter = {'R_Task_Request_M': 0, 'R_Task_No_W': 0,
+                       'R_Operator_ASCII': [0] * len(self.tool_addresses['R_Operator_ASCII'])}
+            print('RESET FOR ASSEMBLY', reseter)
+        return reseter
+
+    def send_response_PLC(self, response):
+        markers_addresses, markers_values = self.separate_response(response, 'marker')
+        words_addresses, words_values = self.separate_response(response, 'word')
+        dwords_addresses, dwords_values = self.separate_response(response, 'dword')
+        print(markers_addresses, markers_values)
+        print(words_addresses, words_values)
+        print(dwords_addresses, dwords_values)
+        # self.connect()
+        # self.write_random_bits(markers_addresses, markers_values)
+        # self.write_random_words(word_devices=words_addresses, word_values=words_values,
+        #                         double_word_devices=dwords_addresses, double_word_values=dwords_values)
+        # self.close_connection()
+
+    def separate_response(self, response, type_of_separation):
+        print('\n\nRESPONSE', response, type_of_separation)
+        if type_of_separation == 'marker':
+            separated = {key: value for key, value in response.items() if key.endswith('M')}
+            separated_keys = list({self.tool_addresses.get(key, key): value for key, value in separated.items()}.keys())
+            separated_values = (
+                list({self.tool_addresses.get(key, key): value for key, value in separated.items()}.values()))
+        else:
+            if type_of_separation == 'word':
+                separated = \
+                    {key: value for key, value in response.items() if key.endswith('_W') or key.endswith('ASCII')}
+            if type_of_separation == 'dword':
+                separated = {key: value for key, value in response.items() if key.endswith('DW')}
+            separated_keys = [self.tool_addresses[key] for key in separated.keys()]
+            separated_keys = [item if not isinstance(sublist, list) else item for sublist in separated_keys for
+                              item in (sublist if isinstance(sublist, list) else [sublist])]
+            separated_values = [separated[key] for key in separated.keys()]
+            separated_values = [item if not isinstance(sublist, list) else item for sublist in separated_values for
+                                item in (sublist if isinstance(sublist, list) else [sublist])]
+        print(type_of_separation, separated_keys, separated_values)
+        return separated_keys, separated_values
+
+    def log_task_to_sql(self, task, tool_name, tool_data, operator_name):
+        LogToolID = tool_data['DefID']
+        LogLineId = tool_data['AdrIDLine']
+        LogLineMachineID = tool_data['AdrIDMachine']
+        current_datetime = datetime.datetime.now()
+        LogDate = current_datetime.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        LogDescription = (
+            f'\'{tool_name} | '
+            f'RepQty: {tool_data["DefReplacementQty"]} | '
+            f'ProdQty: {tool_data["DefProducedQty"]} | '
+            f'Confirmation: {tool_data["DefConfirmation"]} | '
+            f'Release: {tool_data["DefRelease"]} | '
+            f'Status: {tool_data["StatsDescription"]}\''
+        )
+        LogUserLogin = operator_name
+
+        if task == 1:
+            LogType = 4
+
+        if task == 2:
+            LogType = 3
+
+        query = (f'INSERT INTO tToolLogs (LogToolID, LogLineId, LogMachineID, LogDate, LogType, LogDescription, '
+                 f'LogUserLogin) VALUES ({LogToolID}, {LogLineId}, {LogLineMachineID}, \'{LogDate}\', {LogType}, '
+                 f'{LogDescription}, \'{LogUserLogin}\')')
+        self.execute_query(query, 'INSERT')
+
+    def execute_query(self, query, type_of_query, range_of_query=None):
+        if type_of_query == 'SELECT':
+            result = self.cursor_execution(query, type_of_query, range_of_query)
+            return result
+        if type_of_query == 'INSERT' or type_of_query == 'UPDATE':
+            self.cursor_execution(query, type_of_query, range_of_query)
+
+    def cursor_execution(self, query, type_of_query, range_of_query):
+        print('\n', query, '\n')
+        cnxn = pyodbc.connect(self.conn_string, timeout=1)
+        cursor = cnxn.cursor()
+        cursor.execute(query)
+
+        if type_of_query == 'SELECT':
+            columns = [column[0] for column in cursor.description]
+            if range_of_query == 'one':
+                result = cursor.fetchone()
+                if result is None:
+                    raise Exception(f'Response None from SQL for fetch tool query: {query}')
+                zipped = [dict(zip(columns, result))]
+
+        if type_of_query == 'INSERT' or type_of_query == 'UPDATE':
+            cursor.commit()
+            zipped = None
+
+        del cnxn
+        return zipped
+
+
+    @staticmethod
+    def convert_to_ascii(decimal_list):
+        binary = [bin(b).replace('0b', '').zfill(16) for b in decimal_list]
+        halves_list = [half for word in binary for half in (word[len(word) // 2:], word[:len(word) // 2])]
+        ascii_string = ''.join([chr(int(binary, 2)) for binary in halves_list])
+        ascii_string_result = re.sub(r'[^a-zA-Z0-9-]', '', ascii_string)
+        return ascii_string_result
+
+
+
+
---
+++ main.py
@@ -0,0 +1,98 @@
+from base import Tool
+from conf import connstring
+import pyodbc
+import json
+import datetime
+
+
+def get_tool_locations(connstring):
+    string = 'SELECT * FROM tToolAdr'
+    cnxn = pyodbc.connect(connstring, timeout=1)
+    cursor = cnxn.cursor()
+    cursor.execute(string)
+    result = cursor.fetchall()
+    columns = [column[0] for column in cursor.description]
+    del cnxn
+    zipped_tool_locations = [dict(zip(columns, record)) for record in result]
+    zipped_tool_locations_with_names = [{record['AdrName']: record} for record in zipped_tool_locations]
+    log_actual_tools(zipped_tool_locations_with_names)
+    return zipped_tool_locations_with_names
+
+
+def create_tools(tools_locations):
+    tools_list = []
+    for record in tools_locations:
+        for k, v in record.items():
+            print('K', k)
+            k = Tool(location_name=v['AdrName'], location_ip=v['AdrIP'], location_port=v['AdrPortNo'],
+                     location_network=v['AdrNetworkNo'], location_network_plc=v['AdrNetworkPLCNo'],
+                     location_id_line=v['AdrIDLine'], location_id_machine=v['AdrIDMachine'],
+                     tool_addresses=v['AdrPLC'], conn=connstring)
+            tools_list.append(k)
+    return tools_list
+
+
+def log_actual_tools(zipped_tool_locations_with_names):
+    date = str(datetime.datetime.now())
+    with open('tools_construtors.txt', 'a') as file:
+        file.write(date + '\n')
+        for tool in zipped_tool_locations_with_names:
+            file.write(json.dumps(tool) + '\n')
+
+
+def log_error(e):
+    date = str(datetime.datetime.now())
+    with open('errors.txt', 'a') as file:
+        file.write('\n' + date + '\n' + str(e))
+
+
+tool_locations_defined_in_sql = get_tool_locations(connstring)
+tools = create_tools(tool_locations_defined_in_sql)
+
+
+for i in range(0, 1):
+#for tool in tools:
+    tool = tools[-1]
+
+    try:
+        trigger, task = tool.check_trigger()
+    except:
+        continue
+
+    if trigger:
+        try:
+            tool_name = tool.get_tool_name_PLC()
+            operator_name = tool.get_operator_name_PLC()
+            actual_counter = tool.get_actual_counter_PLC()
+            print(f'tool name: {tool_name}, operator_name: {operator_name}, actual counter: {actual_counter}')
+            if task == 1:
+                #demontaz
+
+                tool_data = tool.get_tool_data_SQL(tool_name)
+                #tool.update_tool_data_SQL(tool_data, actual_counter)
+                response_writable_to_plc = tool.generate_writable_plc_response(tool_data, task, error=0)
+                print('\nresponse_writable_to_plc', response_writable_to_plc)
+                reset_readable_to_plc = tool.generate_reset_plc_signals(task)
+                print('response_readable_to_plc', reset_readable_to_plc)
+                response_to_plc = {**response_writable_to_plc, **reset_readable_to_plc}
+                print('\nGENERATED RESPONSE FOR PLC', response_to_plc)
+
+            elif task == 2:
+                #montaz
+
+                tool_data = tool.get_tool_data_SQL(tool_name)
+                response_writable_to_plc = tool.generate_writable_plc_response(tool_data, task, error=0)
+                print('response_writable_to_plc', response_writable_to_plc)
+                reset_readable_to_plc = tool.generate_reset_plc_signals(task)
+                print('response_readable_to_plc', reset_readable_to_plc)
+                response_to_plc = {**response_writable_to_plc, **reset_readable_to_plc}
+                print('response', response_to_plc)
+
+            # tool.send_response_PLC(response_to_plc)
+            #
+            # tool.log_task_to_sql(task, tool_name, tool_data, operator_name)
+
+        except Exception as e:
+            log_error(e)
+            response_to_plc = tool.generate_writable_plc_response(None, None, error=1)
+            #tool.send_response_PLC(response_to_plc)
